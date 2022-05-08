b2d3e8b3a5530d395bffbd2bed8d025011e25a89
pichugina-v <Psyde26@yandex.ru>
2022-05-08 16:39:35 +0300

    added: error_msg, readme; fixed: annotation

--- webapp/api/views.py
+++ webapp/api/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request
-from typing import Tuple, Union
+from typing import Dict, Tuple, Union
 
 from .models import QuizInfo
 from .schemas import QuestionSchema
@@ -7,16 +7,21 @@ from .utils import get_and_save_response
 
 blueprint = Blueprint('home', __name__)
 
+ZERO_VALUE_ERROR = "Value must be greater than 0"
+DATA_TYPE_ERROR = "Incorrect data type, must be integer instead"
+
 
 @blueprint.route('/api', methods=['POST'])
-def index() -> Tuple[str, int]:
+def index() -> Union[Tuple[str, int], Tuple[Dict[str, str], int]]:
     num: Union[int, str]
-    
+
     num = request.form['questions_num']
     try:
         num = int(num)
+        if num == 0:
+            return {"error_message": ZERO_VALUE_ERROR}, 400
         last_saved_question = QuizInfo.query.order_by(-QuizInfo.id).first()
         get_and_save_response(num)
         return QuestionSchema().jsonify(last_saved_question), 200
     except ValueError:
-        return 'Incorrect data type, must be integer instead', 400
+        return {"error_message": DATA_TYPE_ERROR}, 400
--- webapp/config.py
+++ webapp/config.py
@@ -4,7 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = (f'postgresql://{os.getenv("POSTGRES_USER")}:'
                            f'{os.getenv("POSTGRES_PASSWORD")}@'
-                           f'db:{os.getenv("POSTGRES_PORT")}/'
+                           f'{os.getenv("POSTGRES_HOST")}:'
+                           f'{os.getenv("POSTGRES_PORT")}/'
                            f'{os.getenv("POSTGRES_DB")}')
-
-SECRET_KEY = os.getenv('SECRET_KEY')
